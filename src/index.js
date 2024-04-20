ac36a70fae15c51277d438e2ecbf3fbe21b5fc67
Raj kumar <drajkumar01234@gmail.com>
2024-04-20 11:49:00 +0530

    asynchandler

--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path:"./env"
 })
 
 connectDB
-
-
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log("SERVER IS RUNNING")
+    })
+})
+.catch((error)=>{
+    console.log("DB CONNECTION FAILED",error)
+})
 
 
 
